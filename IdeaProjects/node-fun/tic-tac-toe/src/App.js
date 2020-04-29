82f1f14d6a8fa775a3f568fdd964cb03734a5918
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-04-29 13:56:38 -0500

    adding components to react tic tac toe

---
+++ IdeaProjects/node-fun/tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tic Tac Toe!</h1>
+        <h3>Ready to play?</h3>
+    </div>
+  );
+}
+
+export default App;
